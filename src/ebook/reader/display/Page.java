1c0a69123d1c3299e5e1d0275b9050df828157a3
Wilson Hayward <>
2016-05-13 14:00:27 -0500

    Added the page class

---
+++ src/ebook/reader/display/Page.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ebook.reader.display;
+public class Page {
+    private int pageNumber;
+    private String pageText;
+    
+    public Page(String pageT, int pageN){
+        this.pageNumber = pageN;
+        this.pageText = pageT;
+    }
+    
+    public int getPageNumber(){
+        return pageNumber;
+    }
+    
+    public String getPageText(){
+        return pageText;
+    }
+    
+}
