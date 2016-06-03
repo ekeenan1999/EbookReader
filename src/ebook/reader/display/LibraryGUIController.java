b25edfb85e17ca7526102bea784044eff00d4ca1
ekeenan <ekeenan@10.150.32.241>
2016-06-03 13:13:00 -0500

    all imageviews/labels in the library should work when clicked on

--- src/ebook/reader/display/LibraryGUIController.java
+++ src/ebook/reader/display/LibraryGUIController.java
@@ -34,6 +34,16 @@ public class LibraryGUIController implements Initializable {
     
     private boolean b1pressed = false;
     
+    private boolean b2pressed = false;
+     
+    private boolean b3pressed = false;
+      
+    private boolean b4pressed = false;
+       
+    private boolean b5pressed = false;
+        
+    private boolean b6pressed = false;
+    
     @FXML
     private ImageView book1;
     
@@ -109,13 +119,142 @@ public class LibraryGUIController implements Initializable {
             Singleton.saveBook(newBook);
             b1pressed = true;
         } else if (b1pressed == true) {
-            Singleton.recallBook();
+            //Singleton.recallBook();
             handleBackToBook();
             
-            //OPEN BOOK THAT IS SAVED IN THE PANEL
+            //OPEN BOOK THAT IS SAVED IN THE LIBRARY
+        }
+    }
+    
+     @FXML 
+    public void onMouseClickedB2() {
+        if(b2pressed ==false) {
+            Scanner userInput = new Scanner(System.in);
+            String url = null;
+            System.out.println("Please enter in the url of the book you want to add.");
+            url = userInput.nextLine();
+            userInput.nextLine();
+            String title = null;
+            System.out.println("Please enter the title of the book");
+            title = userInput.nextLine();
+            String author = null;
+            System.out.println("Please enter the author of the book");
+            author = userInput.nextLine();
+            Book newBook = new Book(url, title, author);
+            b1Label.setText(newBook.getTitle() + " by " + newBook.getAuthor());
+            Singleton.saveBook(newBook);
+            b2pressed = true;
+        } else if (b2pressed == true) {
+            //Singleton.recallBook();
+            handleBackToBook();
+            
+            //OPEN BOOK THAT IS SAVED IN THE LIBRARY
+        }
+    }
+    
+     @FXML 
+    public void onMouseClickedB3() {
+        if(b3pressed ==false) {
+            Scanner userInput = new Scanner(System.in);
+            String url = null;
+            System.out.println("Please enter in the url of the book you want to add.");
+            url = userInput.nextLine();
+            userInput.nextLine();
+            String title = null;
+            System.out.println("Please enter the title of the book");
+            title = userInput.nextLine();
+            String author = null;
+            System.out.println("Please enter the author of the book");
+            author = userInput.nextLine();
+            Book newBook = new Book(url, title, author);
+            b1Label.setText(newBook.getTitle() + " by " + newBook.getAuthor());
+            Singleton.saveBook(newBook);
+            b3pressed = true;
+        } else if (b3pressed == true) {
+            //Singleton.recallBook();
+            handleBackToBook();
+            
+            //OPEN BOOK THAT IS SAVED IN THE LIBRARY
+        }
+    }
+    
+     @FXML 
+    public void onMouseClickedB4() {
+        if(b4pressed ==false) {
+            Scanner userInput = new Scanner(System.in);
+            String url = null;
+            System.out.println("Please enter in the url of the book you want to add.");
+            url = userInput.nextLine();
+            userInput.nextLine();
+            String title = null;
+            System.out.println("Please enter the title of the book");
+            title = userInput.nextLine();
+            String author = null;
+            System.out.println("Please enter the author of the book");
+            author = userInput.nextLine();
+            Book newBook = new Book(url, title, author);
+            b1Label.setText(newBook.getTitle() + " by " + newBook.getAuthor());
+            Singleton.saveBook(newBook);
+            b4pressed = true;
+        } else if (b4pressed == true) {
+            //Singleton.recallBook();
+            handleBackToBook();
+            
+            //OPEN BOOK THAT IS SAVED IN THE LIBRARY
+        }
+    }
+    
+     @FXML 
+    public void onMouseClickedB5() {
+        if(b5pressed ==false) {
+            Scanner userInput = new Scanner(System.in);
+            String url = null;
+            System.out.println("Please enter in the url of the book you want to add.");
+            url = userInput.nextLine();
+            userInput.nextLine();
+            String title = null;
+            System.out.println("Please enter the title of the book");
+            title = userInput.nextLine();
+            String author = null;
+            System.out.println("Please enter the author of the book");
+            author = userInput.nextLine();
+            Book newBook = new Book(url, title, author);
+            b1Label.setText(newBook.getTitle() + " by " + newBook.getAuthor());
+            Singleton.saveBook(newBook);
+            b5pressed = true;
+        } else if (b5pressed == true) {
+            //Singleton.recallBook();
+            handleBackToBook();
+            
+            //OPEN BOOK THAT IS SAVED IN THE LIBRARY
+        }
+    }
+    
+     @FXML 
+    public void onMouseClickedB6() {
+        if(b6pressed ==false) {
+            Scanner userInput = new Scanner(System.in);
+            String url = null;
+            System.out.println("Please enter in the url of the book you want to add.");
+            url = userInput.nextLine();
+            userInput.nextLine();
+            String title = null;
+            System.out.println("Please enter the title of the book");
+            title = userInput.nextLine();
+            String author = null;
+            System.out.println("Please enter the author of the book");
+            author = userInput.nextLine();
+            Book newBook = new Book(url, title, author);
+            b1Label.setText(newBook.getTitle() + " by " + newBook.getAuthor());
+            Singleton.saveBook(newBook);
+            b6pressed = true;
+        } else if (b6pressed == true) {
+            //Singleton.recallBook();
+            handleBackToBook();
+            
+            //OPEN BOOK THAT IS SAVED IN THE LIBRARY
         }
     }
     
 }
 
-//Add labels to scene builder in imageviews... then add image of book to imageviews.. change label onMouseClicked
